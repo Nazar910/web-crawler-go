35b8cfa47599c2b2a62c290681fd6ffa89ac78e7
Nazar910 <Nazar910@users.noreply.github.com>
2026-03-29 11:24:09 +0200

    [crawler]: return nil instead of []string{} when got error in processLink

--- crawler.go
+++ crawler.go
@@ -45,15 +45,15 @@ type crawler struct {
 
 func (c *crawler) start() {
 	for link := range c.urlch {
-		c.results <- c.eachl(link)
+		c.results <- c.processLink(link)
 	}
 }
 
-func (c *crawler) eachl(link string) []string {
+func (c *crawler) processLink(link string) []string {
 	urlLink, err := url.Parse(link)
 
 	if err != nil {
-		return []string{}
+		return nil
 	}
 
 	fmt.Println(link)
@@ -61,14 +61,14 @@ func (c *crawler) eachl(link string) []string {
 
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
-		return []string{}
+		return nil
 	}
 
 	res, err := c.client.Do(req)
 
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
-		return []string{}
+		return nil
 	}
 
 	defer res.Body.Close()
@@ -76,7 +76,7 @@ func (c *crawler) eachl(link string) []string {
 
 	if err != nil {
 		fmt.Printf("html parse error: %v\n", err)
-		return []string{}
+		return nil
 	}
 
 	return slices.AppendSeq(
